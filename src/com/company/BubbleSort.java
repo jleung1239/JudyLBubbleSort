d6c1ca85d271a3b5572136037f491bf83a48a1c2
BT_1N3_13 <a>
2019-01-02 08:39:59 -0500

    a

--- src/com/company/BubbleSort.java
+++ src/com/company/BubbleSort.java
@@ -4,6 +4,10 @@ public class BubbleSort {
     public static void bubbleSort(int[] arr){
         int swap = 1;
         int temp;
+        
+        //sorts the list in order from least to greatest
+        //if a number is out of place, it swaps them
+        //if no swaps have been made when checking through a list, then it stops
         while (swap > 0){
             swap = 0;
             for (int i = 1;i < arr.length; i++){
